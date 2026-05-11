a87a41d2967a0333db5d9d26d0511c3086125fe1
kashifali-ai <kashifali.jhu@gmail.com>
2026-05-11 19:18:56 +0530

    Fixed frontend API URL

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-  baseURL: "https://task-management-system-production-dceb.up.railway.app"
+  baseURL: "https://task-management-system-production-dceb.up.railway.app/"
 });
 
 export default API;
